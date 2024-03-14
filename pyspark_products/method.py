7a127d6cd0349cd44aa0c7aa96ee188c78a44021
«Selezneva <petrukhina.marina2011@yandex.ru>
2024-03-14 14:53:18 +0300

    lint

--- pyspark_products/method.py
+++ pyspark_products/method.py
@@ -1,13 +1,12 @@
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType
-
-
 from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
+
+
 sc = SparkContext.getOrCreate()
 spark = SparkSession(sc)
-
+# Пример продуктов, категорий и их связей
 products_data = [
-    (1, "product_1"),    #пример продуктов, категорий и их связей 
+    (1, "product_1"),
     (2, "product_2"),
     (3, "product_3")
     ]
@@ -23,18 +22,17 @@ df_categories = ['category_id', 'name_category']
 categories = spark.createDataFrame(categories_data, df_categories)
 
 links_data = [(1, 1, 1),
-         (2, 1, 2),
-         (3, 2, 2)
-         ]
+              (2, 1, 2),
+              (3, 2, 2)
+              ]
 df_links = ['link_id', 'category_id', 'product_id']
 links = spark.createDataFrame(links_data, df_links)
-
-df = products.join(links, "product_id", 'left')       #сам джоин
+# Сам джоин
+df = products.join(links, "product_id", 'left')
 df_rez = df.join(categories, 'category_id', 'left')
 df_rez.select("name_product", "name_category").show()
 
-#Вывод получается вот такой
-
+# Вывод ниже
 # +------------+-------------+                                                    
 # |name_product|name_category|
 # +------------+-------------+
