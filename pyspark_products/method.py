1d3d3bdd176e3cbcf2cc02592ebb68c25f4976f6
«Selezneva <petrukhina.marina2011@yandex.ru>
2024-03-14 14:25:21 +0300

    method.py

--- pyspark_products/method.py
+++ pyspark_products/method.py
@@ -1,7 +1,13 @@
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
+
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+sc = SparkContext.getOrCreate()
+spark = SparkSession(sc)
+
 products_data = [
-    (1, "product_1"),
+    (1, "product_1"),    #пример продуктов, категорий и их связей 
     (2, "product_2"),
     (3, "product_3")
     ]
@@ -16,13 +22,24 @@ categories_data = [
 df_categories = ['category_id', 'name_category']
 categories = spark.createDataFrame(categories_data, df_categories)
 
-links_data = [(1, "category_1", "product_1"),
+links_data = [(1, 1, 1),
          (2, 1, 2),
          (3, 2, 2)
          ]
 df_links = ['link_id', 'category_id', 'product_id']
 links = spark.createDataFrame(links_data, df_links)
 
-df = products.join(categories, "product_id", 'left')
-df_rez = df.join(links, 'category_id', 'left')
+df = products.join(links, "product_id", 'left')       #сам джоин
+df_rez = df.join(categories, 'category_id', 'left')
 df_rez.select("name_product", "name_category").show()
+
+#Вывод получается вот такой
+
+# +------------+-------------+                                                    
+# |name_product|name_category|
+# +------------+-------------+
+# |   product_1|   category_1|
+# |   product_2|   category_2|
+# |   product_2|   category_1|
+# |   product_3|         NULL|
+# +------------+-------------+
